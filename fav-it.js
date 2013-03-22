de0547ba99d48da2f270aef6610760c1b191916d
sebastian-torres <sebastian@wineducation.com.ar>
2013-03-22 10:24:54 -0300

    Remove unused jaavascript

--- fav-it.js
+++
@@ -1,9 +0,0 @@
-//Favorite this image and store favs as cookie - ver 1.0 
-
-function FAVit() {
-	var img_id = 0;
-	
-	$( '.photo' ).each(function( index ) {
-	  $(this).append('<div class="fav-it">&nbsp;</div>');
-	});
-}
